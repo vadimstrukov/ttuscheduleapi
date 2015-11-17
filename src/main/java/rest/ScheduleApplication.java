90b22f62ee01dd889da05465f354ef8d57722494
vadimstrukov <vadim.strukov@gmail.com>
2015-11-17 15:17:31 +0200

    Initial commit

---
+++ src/main/java/rest/ScheduleApplication.java
@@ -0,0 +1,15 @@
+package rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vadimstrukov on 11/17/15.
+ */
+@SpringBootApplication
+public class ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
+}
