ab444b342032af264b0879267f4acf411048e517
Fjodor Vershinin <fedor@vershinin.net>
2015-12-04 14:28:30 +0200

    Added Tomcat Support

--- src/main/java/rest/ScheduleApplication.java
+++ src/main/java/rest/ScheduleApplication.java
@@ -2,14 +2,18 @@ package rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-/**
- * Created by vadimstrukov on 11/17/15.
- */
 @SpringBootApplication
-public class ScheduleApplication {
+public class ScheduleApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ScheduleApplication.class);
     }
 }
