c0f19115e60e131ae1e4ff39d1a4216163686542
Fjodor Vershinin <fedor@vershinin.net>
2015-12-04 13:36:52 +0200

    Fixed TTUSchedule(at now sync return map of claendars)  and getting multiple schedules

--- src/main/java/rest/client/TTUSchedule.java
+++ src/main/java/rest/client/TTUSchedule.java
@@ -1,6 +1,5 @@
 package rest.client;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
@@ -22,25 +21,21 @@ public class TTUSchedule {
     private static final String GROUPS_URL = "https://ois.ttu.ee/portal/page?_pageid=35,435155&_dad=portal&_schema=PORTAL&i=2&e=-1&e_sem=161&a=1&b=%1$d&c=-1&d=-1&k=&q=neto&g=";
     private static final String CALENDAR_URL = "https://ois.ttu.ee/pls/portal/tunniplaan.PRC_EXPORT_DATA?p_page=view_plaan&pn=i&pv=2&pn=e_sem&pv=161&pn=e&pv=-1&pn=b&pv=1&pn=g&pv=%1$d&pn=is_oppejoud&pv=false&pn=q&pv=1";
 
-    private List<Calendar> calendars;
     private Map<String, Integer> groupsMap;
 
     public TTUSchedule() throws IOException {
         System.setProperty("https.protocols", "TLSv1,SSLv3,SSLv2Hello");
-        calendars = Lists.newLinkedList();
         groupsMap = getGroupsMap();
     }
 
-    public void sync(List<String> groups) throws IOException, ParserException {
+    public Map<String, Calendar> getCalendars(List<String> groups) throws IOException, ParserException {
+        Map<String, Calendar> calendarMap = Maps.newHashMap();
         for (String group : groups){
             URL url = new URL(String.format(CALENDAR_URL, groupsMap.get(group)));
             CalendarBuilder calendarBuilder = new CalendarBuilder();
-            calendars.add(calendarBuilder.build(url.openConnection().getInputStream()));
+            calendarMap.put(group, calendarBuilder.build(url.openConnection().getInputStream()));
         }
-    }
-
-    public List<Calendar> getCalendars() {
-        return calendars;
+        return calendarMap;
     }
 
     public Set<String> getAllGroups(){
--- src/main/java/rest/controller/ScheduleController.java
+++ src/main/java/rest/controller/ScheduleController.java
@@ -4,35 +4,25 @@ import com.google.common.collect.Lists;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import rest.client.TTUSchedule;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
-@RequestMapping("/groups")
+@RequestMapping("/schedule")
 public class ScheduleController {
 
     @Autowired
     private TTUSchedule ttuSchedule;
 
-    @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody
-    List<Calendar> getSchedule(@RequestParam(value="group", required=true) String group) throws IOException, ParserException {
-        ttuSchedule.sync(Lists.newArrayList(group));
-        return ttuSchedule.getCalendars();
+
+    @RequestMapping
+    public ResponseEntity<Map<String, Calendar>> Schedules(@RequestParam(value="group", required=true) String[] group) throws IOException, ParserException {
+        return new ResponseEntity<>(ttuSchedule.getCalendars(Lists.newArrayList(group)), HttpStatus.OK);
     }
 }
