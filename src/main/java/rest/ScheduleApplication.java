6793d3f248b6d97236a3b8cce105840f148a93ba
vadimstrukov <vadim.strukov@gmail.com>
2015-12-04 13:19:24 +0200

    Deploy

--- src/main/java/rest/ScheduleApplication.java
+++ src/main/java/rest/ScheduleApplication.java
@@ -1,19 +1,16 @@
 package rest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class ScheduleApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-
-    private static Class<ScheduleApplication> applicationClass = ScheduleApplication.class;
 
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
