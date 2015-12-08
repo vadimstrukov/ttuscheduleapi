64c3fc33e6ee50cbe0150af6cc4bdb47200b7452
vadimstrukov <vadim.strukov@gmail.com>
2015-12-08 21:03:01 +0200

    Constants.GROUP

--- app/src/main/java/ee/ttu/schedule/utils/Constants.java
+++ app/src/main/java/ee/ttu/schedule/utils/Constants.java
@@ -1,9 +1,9 @@
 package ee.ttu.schedule.utils;
 
 public class Constants {
+    public static final String GROUP = "group";
     public static final String SYNCHRONIZATION_ACTION = "ee.ttu.schedule.SYNC_FINISHED";
     public static final String SYNC_STATUS = "sync_status";
     public static final int SYNC_STATUS_FAILED = 0;
     public static final int SYNC_STATUS_OK = 1;
-    public static final String GROUP = "group";
 }
